8f6b618d704ecd12545b526408f214790a179143
Sarah-Kaehne <89110826+Sarah-Kaehne@users.noreply.github.com>
2021-09-26 12:30:13 +1000

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,4 +1,4 @@
-var vg_1 = "week_9_map.vg.json";
+var vg_1 = "js/week_9_map.vg.json";
 vegaEmbed("#choropleth_map", vg_1).then(function(result) {
  // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
 }).catch(console.error);
